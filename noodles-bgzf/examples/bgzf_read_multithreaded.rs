bc3c09c1cd63814b8395115ac2d042e7bb99f2ae
Michael Macias <zaeleus@gmail.com>
2026-07-29 15:17:12 -0500

    bgzf/examples/read_multithreaded: Add optional worker count parameter

--- noodles-bgzf/examples/bgzf_read_multithreaded.rs
+++ noodles-bgzf/examples/bgzf_read_multithreaded.rs
@@ -6,6 +6,8 @@ use std::{
     env,
     fs::File,
     io::{self, BufReader, BufWriter},
+    num::NonZero,
+    thread,
 };
 
 use noodles_bgzf as bgzf;
@@ -14,10 +16,14 @@ fn main() -> io::Result<()> {
     let mut args = env::args().skip(1);
 
     let src = args.next().expect("missing src");
+    let worker_count = args
+        .next()
+        .map(|s| s.parse().expect("invalid worker count"))
+        .unwrap_or_else(|| thread::available_parallelism().unwrap_or(NonZero::<usize>::MIN));
 
     let mut reader = File::open(src)
         .map(BufReader::new)
-        .map(bgzf::io::MultithreadedReader::new)?;
+        .map(|f| bgzf::io::MultithreadedReader::with_worker_count(worker_count, f))?;
 
     let stdout = io::stdout().lock();
     let mut writer = BufWriter::new(stdout);
