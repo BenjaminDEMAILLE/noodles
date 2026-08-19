00469affa2c85b1efe2bb0fd52219cca3590bb15
Michael Macias <zaeleus@gmail.com>
2026-08-19 18:57:40 -0500

    tabix/io/reader/index/reference_sequences/intervals: Add interval count reader

--- noodles-tabix/src/async/io/reader/index/reference_sequences/intervals.rs
+++ noodles-tabix/src/async/io/reader/index/reference_sequences/intervals.rs
@@ -6,9 +6,7 @@ where
     R: AsyncRead + Unpin,
 {
     // n_intv
-    let interval_count = reader.read_i32_le().await.and_then(|n| {
-        usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
+    let interval_count = read_interval_count(reader).await?;
 
     let mut intervals = Vec::with_capacity(interval_count);
 
@@ -23,6 +21,16 @@ where
     }
 
     Ok(intervals)
+}
+
+async fn read_interval_count<R>(reader: &mut R) -> io::Result<usize>
+where
+    R: AsyncRead + Unpin,
+{
+    reader
+        .read_i32_le()
+        .await
+        .and_then(|n| usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)))
 }
 
 #[cfg(test)]
@@ -50,6 +58,33 @@ mod tests {
         ];
         assert!(matches!(
             read_intervals(&mut &src[..]).await,
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_read_interval_count() -> io::Result<()> {
+        assert_eq!(
+            read_interval_count(&mut &[0x00, 0x00, 0x00, 0x00][..]).await?,
+            0
+        );
+        assert_eq!(
+            read_interval_count(&mut &[0x01, 0x00, 0x00, 0x00][..]).await?,
+            1
+        );
+        assert_eq!(
+            read_interval_count(&mut &[0x00, 0x01, 0x00, 0x00][..]).await?,
+            256
+        );
+
+        assert!(matches!(
+            read_interval_count(&mut io::empty()).await,
+            Err(e) if e.kind() == io::ErrorKind::UnexpectedEof
+        ));
+        assert!(matches!(
+            read_interval_count(&mut &[0xff, 0xff, 0xff, 0xff][..]).await,
             Err(e) if e.kind() == io::ErrorKind::InvalidData
         ));
 
--- noodles-tabix/src/io/reader/index/reference_sequences/intervals.rs
+++ noodles-tabix/src/io/reader/index/reference_sequences/intervals.rs
@@ -9,9 +9,7 @@ where
     R: Read,
 {
     // n_intv
-    let interval_count = read_i32_le(reader).and_then(|n| {
-        usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
+    let interval_count = read_interval_count(reader)?;
 
     (0..interval_count)
         .map(|_| {
@@ -19,6 +17,14 @@ where
             read_u64_le(reader).map(bgzf::VirtualPosition::from)
         })
         .collect()
+}
+
+fn read_interval_count<R>(reader: &mut R) -> io::Result<usize>
+where
+    R: Read,
+{
+    read_i32_le(reader)
+        .and_then(|n| usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)))
 }
 
 #[cfg(test)]
@@ -46,6 +52,27 @@ mod tests {
         ];
         assert!(matches!(
             read_intervals(&mut &src[..]),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_interval_count() -> io::Result<()> {
+        assert_eq!(read_interval_count(&mut &[0x00, 0x00, 0x00, 0x00][..])?, 0);
+        assert_eq!(read_interval_count(&mut &[0x01, 0x00, 0x00, 0x00][..])?, 1);
+        assert_eq!(
+            read_interval_count(&mut &[0x00, 0x01, 0x00, 0x00][..])?,
+            256
+        );
+
+        assert!(matches!(
+            read_interval_count(&mut io::empty()),
+            Err(e) if e.kind() == io::ErrorKind::UnexpectedEof
+        ));
+        assert!(matches!(
+            read_interval_count(&mut &[0xff, 0xff, 0xff, 0xff][..]),
             Err(e) if e.kind() == io::ErrorKind::InvalidData
         ));
 
